1c56fc5c020fd8e4cd5436dbf7485d2338b5cda2
Victor Pimenta <vfp@cin.ufpe.br>
2015-11-06 14:21:43 -0300

    Uncommited changes.

---
+++ BQ308/src/submit3/Main.java
@@ -0,0 +1,135 @@
+package submit3;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SuffixTree st = SuffixTree.makeTree("ABRACADABRA", System.out);
+	}
+
+}
+
+class SuffixTree {
+	private Node root;
+	
+	private SuffixTree(Node root){
+		this.root = root;
+	}
+	
+	public static SuffixTree makeTree(String X, PrintStream ps){
+		Node root = new Node(0,0,0);					// Start root and first node
+		root.addChild(new Node(1,1,1));					
+		SuffixTree st = new SuffixTree(root);
+		printTree(st.root, ps);							// Print first tree
+		ps.println("--");
+		int index = 2;
+		
+		for(int i = 2; i<=X.length(); i++){				// Build a ST (x1..xi)
+			char xi = X.charAt(i-1);
+			for(int j = 1; j<=i; j++){					// Create a path for the suffix X[j..i] 
+				Node u;
+				if(j == i) u = st.root;					// At the end of iterations, run for root
+				else u = locus(j,i-1, st.root, X);		// Find the locus of X[j..i-1]
+				
+				Node n = u.next(xi, X);
+				if(n == null){							// If there is no xi after u
+					if(!u.hasChilds()){					// If u is a leaf
+						u.LENGTH += 1;					// Extend label of u as u+xi
+					}else{
+						// Insert a new node w = X[j..i] and a edge (u,w)
+						u.addChild(new Node(index,i,1));
+						index++;
+					}
+				}else if(n.LENGTH > 1){					// If there is xi, bifurcate path
+					Node newChild = new Node(index,n.BEGIN, 1);
+					n.BEGIN++;							// Allow nested search
+					n.LENGTH--;
+					newChild.addChild(n);
+					u.addChild(newChild);
+					index++;
+				}else{
+					u.addChild(n);
+				}
+			}
+			
+			printTree(st.root, ps); 					// Print tree after each step	
+			ps.println("--");
+		}
+		
+		return st;
+	}
+
+	private static void printTree(Node e, PrintStream ps) {
+		String origin = "ORIGEM: ";
+		String target = "; DESTINO: ";
+		String label = "; ROTULO: ";
+		
+		if(e.hasChilds()){
+			for(Node node : e.getChilds()){
+				ps.println(origin+e.ID+target+node.ID+label+node.BEGIN+","+node.LENGTH);
+				printTree(node, ps);
+			}
+		}
+	}
+
+	private static Node locus(int begin, int end, Node e, String str) {
+		if(e.BEGIN > 0 && str.substring(e.BEGIN-1, e.BEGIN+e.LENGTH-1).equals(str.substring(begin-1, end))) return e;
+		else if(e.hasChilds()){
+			for (Node node : e.getChilds()) {
+				String child = str.substring(node.BEGIN-1, node.BEGIN+node.LENGTH-1);
+				String search = str.substring(begin-1+e.LENGTH, end);
+				if(search.startsWith(child)){
+					Node x = locus(begin+e.LENGTH,end,node,str);
+					if(x != null) return x;
+				}
+			}
+		}else{
+			return null;
+		}
+		return null;
+	}
+}
+
+class Node{
+	public int ID;
+	public int BEGIN;
+	public int LENGTH;
+	private List<Node> childs;
+	
+	public Node(int id, int begin, int length){
+		this.ID = id;
+		this.BEGIN = begin;
+		this.LENGTH = length;
+		this.childs = new ArrayList<Node>();
+	}
+	
+	public void addChild(Node e){
+		this.childs.add(e);
+	}
+
+	public List<Node> getChilds() {
+		return childs;
+	}
+	
+	public boolean hasChilds(){
+		return childs.size() > 0;
+	}
+	
+	public Node next(char ch, String str){
+		if(!this.hasChilds()) return null;
+		else{
+			for (Node node : childs) {
+				if(str.charAt(node.BEGIN-1) == ch){
+					Node acc = node;
+					childs.remove(node);
+					return acc;
+				}
+			}
+		}
+		return null;
+	}
+	
+}
--- BQ308/src/sufixTree/Main.java
+++ BQ308/src/sufixTree/Main.java
@@ -3,7 +3,7 @@ package sufixTree;
 public class Main {
 
 	public static void main(String[] args) {
-		SuffixTree st = SuffixTree.makeTree("ABRACADABRA", System.out);
+		SuffixTree st = SuffixTree.makeTree("BANANA", System.out);
 	}
 
 }
